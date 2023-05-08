53a0226d03218054de5476170a3f47ebaeee8808
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-08 19:12:03 -0300

    ✨ Feat: Added routes to app

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
 import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
+import { router } from './routes'
 
 const PORT = process.env.PROT ?? 3000
 const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(router)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
