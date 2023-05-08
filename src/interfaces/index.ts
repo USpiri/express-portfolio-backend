e3c40a965abe4fd17d9af6b7edf872e35028983a
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-08 20:32:47 -0300

    ✨ Feat: Added User interface

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,1 +1,2 @@
 export * from './handlers.interface'
+export * from './user.interface'
---
+++ src/interfaces/user.interface.ts
@@ -0,0 +1,10 @@
+export interface User {
+  name: string
+  title: string
+  description: string[]
+  birthDate: Date
+  phone: string
+  email: string
+  location: string
+  image: string
+}
