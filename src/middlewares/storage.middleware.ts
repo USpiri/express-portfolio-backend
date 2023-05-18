19ff07893cd879f578e587a7409dc44140699971
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-18 01:42:34 -0300

    🌿 Refactor: Remove comments

--- src/middlewares/storage.middleware.ts
+++ src/middlewares/storage.middleware.ts
@@ -1,20 +1,7 @@
 import { Request } from 'express'
 import multer, { FileFilterCallback } from 'multer'
 
-// const STORAGE_PATH = `${process.cwd()}/storage`
-
 const storage = multer.memoryStorage()
-
-// const storage = diskStorage({
-//   destination(_req: Request, _file: Express.Multer.File, cb: any) {
-//     cb(null, STORAGE_PATH)
-//   },
-//   filename(_req: Request, file: Express.Multer.File, cb: any) {
-//     const ext = file.originalname.split('.').pop() ?? ''
-//     const filenameRandom = `image-${Date.now()}.${ext}`
-//     cb(null, filenameRandom)
-//   }
-// })
 
 const fileFilter = (_req: Request, file: Express.Multer.File, cb: FileFilterCallback): void => {
   const allowedTypes = ['image/jpeg', 'image/png']
