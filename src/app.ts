a0b14c4cec7e59f09a56d8f020dec2823232d9b7
Uriel Spiridione <urielspiridione135@gmail.com>
2023-05-20 02:53:04 -0300

    🐛 Fix: Removed deprecated value

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ const PORT = process.env.PROT ?? 3000
 const app = express()
 
 app.use(express.json({ limit: '25mb' }))
-app.use(express.urlencoded({ limit: '25mb' }))
 app.use(cors())
 app.use(router)
 app.use('/image', express.static('storage/images'))
